87e9c0f8ac950a7f2ba4860d3c927a23a7388d00
p0lyn0mial <lukasz.szaszkiewicz@gmail.com>
2017-04-24 03:50:37 +0200

    Described a solution for the teaser

--- odd_even_pivot/teaser.go
+++ odd_even_pivot/teaser.go
@@ -7,9 +7,21 @@ package teaser
 // linearly in time with respect to the size of the array.
 //
 // Solution:
-// TODO
 //
-// TODO: implement the soulution
+// | 1 | 2 | 3 | 4 | 5 | 6 | 7
+//   ^                       ^
+//  even                    odd
+//  pointer                 pointer
+//
+// The basic idea is to maintain two pointers.
+// The even pointer placed at the beginning of an array.
+// The odd pointer place at the end of an array.
+//
+// The first step is to advance the even pointer as long as an odd number is detected.
+// The next step is to decrease the odd pointer as long as an even number is detected.
+// Then swap the numbers and start all over again.
+//
+// Solve solves the teaser
 func Solve(numbers []int) []int {
 	return numbers
 }
