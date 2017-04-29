3630e3f61bafd034d45041a033978daab159a291
p0lyn0mial <lukasz.szaszkiewicz@gmail.com>
2017-04-29 17:03:45 +0200

    implemented add, view methods

--- queuing_system/teaser.go
+++ queuing_system/teaser.go
@@ -1,5 +1,13 @@
 package teaser
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
 // Teaser:
 // Design a queuing system. The queue should implement add, view and delete. A
 // viewed message will be invisible to other workers for 1 second unless it is
@@ -14,27 +22,122 @@ package teaser
 //
 // Extra points: Do you see any problems with running this kind of queue in a
 // production envrionment?
-type queue struct{}
+// TODO: get the extra points by providing an answer
+type queue struct {
+	head *node
+	tail *node
+}
 
 // New returns a new instance of the queuing system.
-func New() queue {
-	return queue{}
+func New() *queue {
+	return &queue{head: sentinel(), tail: sentinel()}
 }
 
 // Add adds a message to the queue and returns
-// a unique id for the message
-func (q queue) Add(msg string) string {
-	return "uuid"
+// a unique id (uuidv4) for the message.
+//
+// note:
+//  passing the same message yields two different ids
+//  messages of size greater than 1024 will be rejected
+func (q *queue) Add(msg string) string {
+	if len(msg) > 1024 {
+		return ""
+	}
+	//TODO: add locking
+	uid := uuid.NewV4()
+
+	oldtail := q.tail
+	q.tail = &node{data: msg, id: uid.String(), next: sentinel()}
+	if q.isEmpty() {
+		q.head = q.tail
+	} else {
+		oldtail.next = q.tail
+	}
+
+	return uid.String()
 }
 
-// View returns a message in FIFO order and a hash of the next message in the queue.
+// View returns a message in FIFO order and a sha-256 of the next message in the queue.
 // Returned message will be invisible for 1 second for others.
-func (q queue) View() (hash string, uuid string) {
-	return "", "uuid"
+//
+// note this method uses a timestamp of type time.Now to determine if a message can be seen.
+// the timestamp should not be considered monotonic, time can drift.
+func (q *queue) View() (hash string, uuid string) {
+	//TODO: add locking
+	if q.isEmpty() {
+		return "", ""
+	}
+
+	n := q.head
+	for {
+		if n.isSentinel() {
+			break
+		}
+		if n.canBeSeen() {
+			n.lastSeen = time.Now()
+			return n.next.hash(), n.id
+		}
+		n = n.next
+	}
+	return n.hash(), n.id
 }
 
 // Delete deletes a message from the queue. Returns true if the message was
 // removed within 1 second or false if we were too slow.
-func (q queue) Delete(uuid string) bool {
+func (q *queue) Delete(uuid string) bool {
+	// TODO: add locking
+	if q.isEmpty() {
+		return false
+	}
+	// TODO: input sanitization
 	return false
+}
+
+// isEmpty determines whether the queue is empty
+func (q *queue) isEmpty() bool {
+	return q.head.isSentinel()
+}
+
+// node is a helper type used by the queue
+// provides linked list semantics
+type node struct {
+	data     string
+	id       string
+	next     *node
+	atype    string
+	lastSeen time.Time
+}
+
+// isSentinel determines whether a node is of type sentinel
+func (n node) isSentinel() bool {
+	return n.atype == "sentinel"
+}
+
+// canBeSeen determines if a message can be seen by a caller.
+// a viewed message is invisible for 1 second.
+//
+// note due to floating-point arithmeitc don't expect accuracy
+// exactly 1 second.
+func (n node) canBeSeen() bool {
+	if n.lastSeen.IsZero() {
+		return true
+	}
+	elapsed := time.Since(n.lastSeen)
+	return elapsed.Seconds() > float64(1)
+}
+
+// hash returns sha-256 calculated over a message
+func (n node) hash() string {
+	if n.isSentinel() {
+		return ""
+	}
+	h := sha256.New()
+	h.Write([]byte(n.data))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// sentinel creates a node of type sentinel
+// think path terminator
+func sentinel() *node {
+	return &node{atype: "sentinel", next: &node{atype: "sentinel"}}
 }
--- queuing_system/teaser_test.go
+++ queuing_system/teaser_test.go
@@ -1,6 +1,8 @@
 package teaser_test
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"testing"
 
 	teaser "github.com/teasers/queuing_system"
@@ -17,11 +19,51 @@ func TestAddViewDelete(t *testing.T) {
 		t.Fatalf("mismatch of added msg id = %s, and viewed msg id = %s", mid, mvid)
 	}
 	if len(nextHash) != 0 {
-		t.Fatal("added only one msg but the hash of the next one returned")
+		t.Fatalf("added only one msg but the hash of the next one returned = %s", nextHash)
 	}
 
 	deleted := target.Delete(mid)
 	if !deleted {
 		t.Fatalf("the message = %s was not deleted", mid)
+	}
+}
+
+// TestAddViewMany a test case that adds many messages
+// and then views them.
+func TestAddViewMany(t *testing.T) {
+	target := teaser.New()
+	m1id := target.Add("msg1")
+	m2id := target.Add("msg2")
+	m3id := target.Add("msg3")
+
+	m2Hash, m1vid := target.View()
+	assertMessageId(m1id, m1vid, t)
+	assertMessageHash("msg2", m2Hash, t)
+
+	m3Hash, m2vid := target.View()
+	assertMessageId(m2id, m2vid, t)
+	assertMessageHash("msg3", m3Hash, t)
+
+	nextHash, m3vid := target.View()
+	assertMessageId(m3id, m3vid, t)
+	assertMessageHash("", nextHash, t)
+
+}
+
+func assertMessageId(actual, expected string, t *testing.T) {
+	if actual != expected {
+		t.Fatalf("mismatch of added msg id = %s, and viewed msg id = %s", actual, expected)
+	}
+}
+
+func assertMessageHash(msg, expectedHash string, t *testing.T) {
+	h := sha256.New()
+	h.Write([]byte(msg))
+	actualHash := fmt.Sprintf("%x", h.Sum(nil))
+	if len(msg) == 0 {
+		actualHash = ""
+	}
+	if actualHash != expectedHash {
+		t.Fatalf("mismatch of hashes over the msg = %s, actual = %s, expected = %s", msg, actualHash, expectedHash)
 	}
 }
